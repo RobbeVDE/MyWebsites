90e6714c8fc1b753d5c155fcfeec66245adf0daf
Robbe Vander Eeckt <Robbe Vander Eeckt>
2015-11-27 19:27:47 +0100

    init ²:wq#

---
+++ 3.Javascript/function.js
@@ -0,0 +1,79 @@
+var clickedTime; var createdTime; var reactionTime;
+var bestTime = 9999999;
+
+ 	function getRandomColor() {
+    	var letters = '0123456789ABCDEF'.split('');
+    	var color = '#';
+   	 		for (var i = 0; i < 6; i++ ) {
+        		color += letters[Math.round(Math.random() * 15)];
+   	 		}
+    		return color;
+	}
+
+ 	function makeBox() {
+
+
+ 		var time=Math.random();
+
+ 			time=time*3256;
+
+ 			setTimeout(function() {
+
+ 				if(Math.random()>0.5) {
+
+ 					document.getElementById("box").style.borderRadius="50px";
+
+ 				} else {
+
+ 					document.getElementById("box").style.borderRadius="0px";
+
+ 				}
+
+ 				 var top=Math.random();
+
+ 		 	 		top=top*300;
+
+ 		 	 	 var left=Math.random();
+
+ 		 	 		left=left*500;
+
+ 				document.getElementById("box").style.top=top+"px";
+
+ 				document.getElementById("box").style.left=left+"px"
+
+ 				document.getElementById("box").style.backgroundColor=getRandomColor();
+
+ 				document.getElementById("box").style.display="block";
+
+ 				createdTime=Date.now();
+
+ 			}, time);
+
+ 	}
+
+
+ 		
+
+ 		document.getElementById("box").onclick=function() {
+
+ 			clickedTime=Date.now();
+
+ 			reactionTime=(clickedTime-createdTime)/1000;
+
+ 			document.getElementById("time").innerHTML=reactionTime;			
+
+ 			this.style.display="none";
+
+ 			if(reactionTime<bestTime) {
+
+ 				bestTime=reactionTime;
+
+ 				document.getElementById("bestTimeDisplay").innerHTML=bestTime;
+ 			}
+
+ 			makeBox();
+ 			
+ 		}
+
+ 		makeBox();
+
---
+++ myProjects/index.php
@@ -0,0 +1,409 @@
+<!doctype html>
+<html>
+ <head>
+ 	<title>musicforlife</title>
+
+ 	<meta charset="utf-8" />
+ 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+ 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+
+
+ 	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+ 	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+
+ 	<script  src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+
+
+
+
+ 	<style type="text/css">
+
+ 		body, html{
+ 			position: relative;
+ 			margin-top: 50px;
+ 			margin-left: 0;
+ 			 
+ 		}
+
+ 		#homeBar{
+ 			position: fixed;
+ 			background-color: #64FE2E;
+ 			width: 100%;
+ 			height: 50px;
+ 			top:-1px;
+ 			z-index: 2;
+ 		}
+
+ 		#homeBar h1{
+ 			position:relative;
+ 			top: -30px;
+ 			color: white;
+ 			float: left;
+ 			margin-left: 10px;
+ 		}
+
+ 		#logo{
+ 			float: left;
+ 			padding: 2px;
+ 			position: relative;
+ 			top: -16px;
+ 		}
+
+
+ 		#praktisch li{
+ 			float: right;
+ 			color: white;
+ 			padding: 5px;
+ 			list-style:none;
+ 			font-size: 1.2em;
+ 			font-weight: bold;
+ 			position: relative;
+ 			top: -5px;
+ 			height: 25px;
+ 			border-right: 1px solid white;
+ 		}
+
+ 		#praktisch a{
+ 			color: white;
+ 			text-decoration: none;
+ 		}
+
+ 		#praktisch li:hover{
+ 			border-bottom: 5px white solid;
+
+ 		}
+
+ 		.fixedwidth{
+ 			margin: 0 auto;
+ 			width:83%;
+ 		}
+
+
+ 		.uitleg{
+ 			border-bottom: grey 1px solid;
+ 			width: 70%;
+ 			color: #357F1B;
+ 		}
+
+ 		.uitleg a{
+ 			text-decoration: none;
+ 			color: #357F1B;
+ 		}
+
+ 		.uitleg a:hover{
+ 			text-decoration: underline;
+ 		}
+
+ 		#contact{
+ 			background-color:#2E2E2E;
+ 			width: 100%;
+ 			height: 150px;
+ 			color: #357F1B;
+ 			padding: 5px 0 0 10px;
+ 		}
+
+ 		#contact a{
+ 			color: #357F1B;
+ 			text-decoration: none;
+ 		}
+
+ 		#contact a:hover{
+ 			text-decoration: underline;
+ 		}
+
+ 		 .ui-accordion-content{
+ 			color: #357F1B; 
+ 		}
+
+ 		.ui-accordion-header{
+ 			color: #357F1B;
+ 		}
+
+ 	</style>
+
+ </head>
+ <body>
+
+ 		<a name="#top"></a>
+
+ 		<div id="homeBar">
+
+ 			<div class="fixedwidth">
+
+ 				<div id="logo">
+ 					
+ 					<a href="#top"><img src="images/academie_logo.jpg" height="47" width="141"></a>
+
+ 				</div>
+
+ 				<h1>Words for life</h1>
+
+ 				<ul id="praktisch">
+ 					
+ 					<li style="border-right:1px solid white"><a href="#boeking">Boekingen</a></li>
+ 					<li  style="border-left:1px solid white"><a href="#contact">Contact</a></li>
+
+
+ 				</ul>
+
+ 			</div>
+
+ 		</div>
+
+
+ 		<div class="uitleg">
+
+ 			<div class="fixedwidth">
+
+ 				
+
+	 			<h2>Wie zijn we?</h2>
+
+	 			<p>De afdeling Woord van de academie Woord,Dans en Muziek Gooik en Herne</p>
+
+	 		</div>
+
+	 	</div>
+
+	 	<div class="uitleg">
+
+ 			<div class="fixedwidth">
+
+	 			<h2>Wat doen we?</h2>
+
+	 			<p>Huiskameroptredens ten voordele van Music for live op 18-19-20 december</p>
+
+	 		</div>
+
+ 		</div>
+
+ 		<div class="uitleg">
+
+ 			<div class="fixedwidth">
+
+	 			<h2>Waarom doen we dit?</h2>
+
+	 			<p>Elk jaar organiseren de woordklassen Gooik en Herne hun optreden in een cultureel centrum. Een optreden in een professionele zaal met spots, techiek. Kortom alles erop en eraan. De keerzijde van de medaille is dat zo’n zaal huren wel wat geld kost. Veel geld. Geld die we eigenlijk zouden kunnen schenken aan een goed doel. Al jaren speelt het in ons hoofd om het geld die we besteden aan kadootjes tijdens ons kerstfeestje te schenken aan Music for life. Maar er werd nooit iets concreets ondernomen. Tot vandaag</p>
+
+	 		</div>
+
+ 		</div>
+
+ 		 	<div class="uitleg">
+
+	 			<div class="fixedwidth">
+
+		 			<h2>Hoe verloopt onze actie?</h2>
+
+		 			<p>De leerlingen zijn volop aan het repeteren voor verschillende huiskamerconcerten van ongeveer 30 min.  Ieder huiskamerconcert heeft een ander thema .
+Wonen jullie in de buurt van Gooik dan klikken jullie door naar Gooik. Ligt Herne meer in jullie omgeving dan klikken jullie door naar Herne.
+Jullie maken een keuze uit de verschillende thema’s en boeken via deze site een huiskamerconcert. ( wij contacteren jullie voor de verdere afspraken )
+U nodigt vrienden en familie uit om het huiskamerconcert  bij te wonen en maakt er een gezellig momentje van . 
+Per aanwezig persoon in de huiskamer wordt er een bijdrage van €3 gevraagd. Op die manier hopen wij van € 1000 te kunnen schenken aan Het Huispaleis.
+Op maandag 21 december willen wij met alle leerlingen dit geld overhandigen en ons plaatje aanvragen.</p>
+
+		 		</div>
+
+		 	</div>
+
+ 		<div class="uitleg">
+
+ 			<div class="fixedwidth">
+
+	 			<a href="http://www.hethuispaleis.be"><h2>vzw Huispaleis</h2></a>
+
+	 			<p>Sinds enkele weken is de woordafdeling druk aan het repeteren om in jullie huiskamer te komen spelen.
+				Samen met de leerlingen gingen we op zoek naar een goed doel. Een goed doel dat moest voldoen aan volgende creteria : 
+				voeling hebben met theater,
+				voor kinderen of jongeren en
+				een minder gekend project
+				Samen hebben we dan besloten om een musicforlive-actie op te starten ten voordele van de vzw Het Huispaleis. Het Huispaleis is een sociaal cultureel artistiek project. In Het Huispaleis staat creativiteit centraal. Iedereen, alle mensen met en zonder een beperking, die hun talent willen ontplooien kunnen in Het Huipaleis terecht.</p>
+
+	 		</div>
+
+ 		</div>
+
+ 			
+ 			<h2>Herne</h2>
+
+ 			<div id="accordionHerne">
+ 				
+ 				<h4>Zoenen</h4>
+ 				<div>Sommige mensen zijn bang op te zoenen. En dat is heel begrijpelijk. Want het is natuurlijk ook niet zomaar iets en je krijgt er ook zelden of nooit voorlichting over. Daarom zijn wij hier.. Om jullie te informeren hoe het nu eigenlijk zit. Zo worden jullie echte kusexperts . ( enfin in theorie toch …. De praktijk? Die moeten jullie zelf uitproberen ;) )
+Bewerking Wie is er bang om te zoenen ? Wim Daniëls Craps Gitte, Delanote Marie, Van Nieuwenhuize Marie  Voordracht M1</div>
+ 				<h4>Kerstmis</h4>
+ 				<div>We houden niet van snoep en van koek en van gember
+We haten Kerst, wij haten december.
+Waarom ?  Zitten jullie ergens scheef?  Of zitten jullie schoenen te strak om de wreef?
+Misschien hebben jullie krampjes? Misschien hebben jullie pijn? 
+Misschien is jullie hart een paar maten te klein ?
+Bewerking: Hoe de Gniep de Kerst stal. Dr Seuss
+Herne: Allaert Eline, Rodrigues Sara, Scuttenaire Sophie en Wellens Lucca Voordracht M1
+
+			</div>
+ 				<h4>familie 2</h4>
+ 				<div></div>
+ 				<h4>Superheld</h4>
+ 				<div>Jij wil dus superheld worden ? Goed nieuws ! Het gaat ook gebeuren, die verandering in jouw leven. Door onze voorstelling zal je weten hoe je superheld moet worden. Maar wat is een superheld eigenlijk?Bewerking: Hoe word ik een superheld ?  Gideaon SamsonHerne: Buckinx Sterre, De Wulf Floris, Vander Eeckt Wannes, Van Kesteren Lerne  Voordracht M2</div>
+ 				<h4>Pesten</h4>
+ 				<div>Stel: ge wordt wakker, ge weet niks meer. Wat zoudt ge doen? Mira wordt gepest. Gruwelijk gepest. Alle dagen. Door CDW, alias Cedric De Winne. De knapste en stoerste van de klas. Op een avond wil hij haar met zijn maten ‘eens goed doen lachen, omdat ze er zo ongelukkig uit ziet’. Het een en ander loopt uit de hand…Tekst: Remember Me Jan Sobrie en Geert VandyckHerne: Carlier Lone, Demol Lola, Vander Eeckt  Robbe en Wellens Jeremy Voordracht M3</div>
+ 				<h4>Vluchtelingen</h4>
+ 				<div>Spreek je Nederlands? vragen ze. 
+Het eerste Nederlandse woord dat ik hoorde was nee. 
+Nee, nee. Overal nee. 
+Ik weet nog dat ik dacht: Ah...ja. 
+Daarom noemen ze het neelands.Bewerking Neeland Nic Balthazar Dedobbeleer Jade,De Wulf Helena, Simons Eugenie Voordracht M3</div>
+ 				<h4>Humor</h4>
+ 				<div></div>
+ 				<h4>In gesprek</h4>
+ 				<div>Een GSM-drama voor jongeren
+Dat is toch raar om niet soms ?
+Ik dacht misschien is het een grap
+Dus ik bel haar
+Krijg ik de volcemail
+Zegt ze iets van: laat me met rust, dankjewel…
+Bewerking : In gesprek Maaike BergstraDe Deken Justine, Vancauwelaert Remke Vannimmen Kaisa  Voordracht  M3/H2/H3</div>
+
+ 			</div>
+
+ 			<h2>Gooik</h2>
+
+ 			<div id="accordionGooik">
+ 				
+ 				<h4>Zoenen</h4>
+ 				<div>Sommige mensen zijn bang op te zoenen. En dat is heel begrijpelijk. Want het is natuurlijk ook niet zomaar iets en je krijgt er ook zelden of nooit voorlichting over. Daarom zijn wij hier.. Om jullie te informeren hoe het nu eigenlijk zit. Zo worden jullie echte kusexperts . ( enfin in theorie toch …. De praktijk? Die moeten jullie zelf uitproberen ;) )
+Bewerking Wie is er bang om te zoenen ? Wim Daniëls
+Gooik: Cerstelotte Mieke, Claes Hanna, Dedobbeleer Inca en Van Craenenbroeck Bauke
+           Voordracht M2</div>
+				<h4>Kerstmis</h4>
+				<div>We houden niet van snoep en van koek en van gember
+We haten Kerst, wij haten december.
+Waarom ?  Zitten jullie ergens scheef?  Of zitten jullie schoenen te strak om de wreef?
+Misschien hebben jullie krampjes? Misschien hebben jullie pijn? 
+Misschien is jullie hart een paar maten te klein ?
+Bewerking: Hoe de Gniep de Kerst stal. Dr Seuss
+Gooik: Claes Hanna, De Coster Evi, White Anaïd   voordracht M2</div>
+				<h4>Superheld</h4>
+				<div>Jij wil dus superheld worden ? Goed nieuws ! Het gaat ook gebeuren, die verandering in jouw leven. Door onze voorstelling zal je weten hoe je superheld moet worden. Maar wat is een superheld eigenlijk?
+Bewerking: Hoe word ik een superheld ?  Gideaon Samson
+Gooik: De Coster Seppe, Hellin Kobe, Slincx Joeri, Van Eeckhoudt Lars  Voordracht M1/M2</div>
+				<h4>Pesten</h4>
+				<div>Stel: ge wordt wakker, ge weet niks meer. Wat zoudt ge doen? Mira wordt gepest. Gruwelijk gepest. Alle dagen. Door CDW, alias Cedric De Winne. De knapste en stoerste van de klas. Op een avond wil hij haar met zijn maten ‘eens goed doen lachen, omdat ze er zo ongelukkig uit ziet’. Het een en ander loopt uit de hand…
+Tekst: Remember Me Jan Sobrie en Geert Vandyck
+Gooik: Peeters Laure, Roelandts Bram, Vankelecom Janne, Van Onckelen Florine 
+           Voordracht M3</div>
+				<h4>Familie 1</h4>
+				<div>VADER moet die jas daar hangen?
+ZOON : ach moeten
+VADER is dat een kapstok ? 
+ZOON..: nee dat is nou een stoel
+VADER :je loopt er verdomme langs , langs de kapstok 
+ZOON : ja ik weet het. Je hoeft mij niet te vertellen wat een kapstok is
+VADER waarom hang je je jas er dan niet aan?
+ZOON .: mijn jas hangt al ergens 
+VADER :maar niet aan de kapstok
+ZOON : nee niet aan de kapstok. hij hangt over een stoel
+ZOO: N dat is een stoel ! geen kapstokl 
+VADER :dat bedoel ik 
+Bewerking: Is dit een kapstok? Esther Gerritsen
+Gooik: De Coster Ine, De Moyter Tyra, Denaeyer Jonas,  Slincx Mirko,Vanoplightenberg Jisca, 
+Voordracht H1</div>
+				<h4>Kanker</h4>
+				<div>Papa, weet je nog dat je me uitlegde wat er in je lijf gebeurde? 
+Je vertelde over gemene soldaatjes. 
+Soldaatjes die alles kapotmaakten. 
+De dokter gaf je een medicijn dat tegen de soldaatjes ging vechten. 
+Het duurde even, en toen won het medicijn. 
+Voor altijd. 
+Dachten we. 
+Je voelde je weer beter. 
+Maar er was nog één soldaatje. 
+Dat had zich heel goed verstopt. 
+
+bewerking: Papa, hoor je me ? Tamara Bos
+
+Gooik: Claes Marie, Parent Joran, Vankelecom Kato  Voordracht H2</div>
+				<h4>In gesprek</h4>
+				<div>Een GSM-drama voor jongeren
+Dat is toch raar om niet soms ?
+Ik dacht misschien is het een grap
+Dus ik bel haar
+Krijg ik de volcemail
+Zegt ze iets van: laat me met rust, dankjewel…
+Bewerking : In gesprek Maaike Bergstra
+Gooik: Claes Marie, Peeters Anja, Vankelecom Kato, Van Praet Nina Voordracht H2</div>
+				<h4>Vluchtelingen</h4>
+				<div>Spreek je Nederlands? vragen ze. 
+Het eerste Nederlandse woord dat ik hoorde was nee. 
+Nee, nee. Overal nee. 
+Ik weet nog dat ik dacht: Ah...ja. 
+Daarom noemen ze het neelands.
+Bewerking Neeland Nic Balthazar 
+Gooik: Billens Joke, Bonte Lore en Breynaert Fien  Voordracht H2/H3</div>
+
+ 			</div>
+
+ 	
+
+ 		<div id="boeking">
+ 			
+ 			<a name="#boeking"></a>
+
+ 				<div class="uitleg">
+
+	 			<div class="fixedwidth">
+
+		 			<h2>Hoe boeken?</h2>
+
+		 			<p>Als je hier te selecteert wanner en welk huisconcert je wil en daarna het geld (3 euro pp)over te storten op het rekeningnummer:"" en in vermelding wie en hoeveel personen en vooral waar, dan zal jouw eigen huiskamerconcert geboekt zijn.</p>
+
+		 		</div>
+
+		 	</div>
+
+ 		</div>
+
+ 		<div id="contact">
+
+ 			<a name="#contact"></a>
+
+ 			<div id="contactTitle">
+ 			
+	 			<h3>Contact</h3>
+
+	 		</div>
+
+	 		<div id="contactList">
+
+	 			<ul>
+	 				
+	 				<li>e-mail:sophie.verbeke@yahoo.com</li>
+	 				<li>telefoon:046442054</li>
+	 			</ul>
+
+	 		</div>
+
+	 		<div id="RoelandtsDev">
+	 			
+	 		<p>&copy <a href="https://www.freelancer.com/u/RobbeVDE.html" target="blank">RoelandtsDev</a></p>
+
+	 		</div>
+
+ 		</div>
+
+
+
+ 		
+ 	</body>
+ 	<script>	
+
+ 		$("#accordion").accordion();
+ 		$("#accordionHerne").accordion();
+ 		$("#accordionGooik").accordion();
+
+ 	</script>
+
+ </body>
+</html>
